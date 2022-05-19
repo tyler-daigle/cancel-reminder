4fd178b0a3479ad48da4c8f15fbafcdc7f5778ee
Tyler D <tyler@tdaigle.com>
2022-05-19 06:14:43 -0400

    MonthlyTotal complete

---
+++ components/MonthlyTotal.jsx
@@ -0,0 +1,18 @@
+import styles from "@styles/components/MonthlyTotal.module.css";
+
+export default function MonthlyTotal({ items }) {
+  const total = totalItems(items);
+
+  return (
+    <div className={styles.monthlyTotalContainer}>
+      <span className={styles.numberItems}>{items.length} items</span>
+      <h3>Total Monthly Cost:</h3>
+      <span className={styles.totalMonthlyCost}>${total}</span>
+    </div>
+  );
+}
+
+function totalItems(items) {
+  const total = items.reduce((total, item) => total + item.price, 0);
+  return total.toFixed(2);
+}
