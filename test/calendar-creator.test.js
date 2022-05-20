80ecad609cb75d408dc9991d36a24e3ac8f98ffe
Tyler D <tyler@tdaigle.com>
2022-05-20 14:33:33 -0400

    calendar creator working

---
+++ test/calendar-creator.test.js
@@ -0,0 +1,41 @@
+import { Calendar, Months, Days } from "../utils/calendar";
+
+// important to remember that Date() starts days and months at 0
+
+describe("The Calendar class is used to create calendars", () => {
+  it("The month of May should have 31 days", () => {
+    const cal = new Calendar(2022);
+    expect(cal.getNumDaysInMonth(Months.MAY)).toEqual(31);
+  });
+
+  it("The 13th of May, 2022, should be a Friday.", () => {
+    const cal = new Calendar(2022);
+    expect(cal.getDayFromDate(Months.MAY, 13)).toEqual(Days.FRIDAY);
+  });
+
+  it("The first day of May 2022 should be a Sunday (0)", () => {
+    const cal = new Calendar(2022);
+    expect(cal.getDayFromDate(Months.MAY, 1)).toEqual(Days.SUNDAY);
+  });
+
+  it("Should be able to create the monthly calendar for May 2022", () => {
+    const cal = new Calendar(2022);
+    const month = cal.createMonthlyCalendar(Months.MAY);
+
+    // there should be 5 weeks created
+    expect(month.length).toEqual(5);
+
+    // in week 1, index 0 should contain the value 1
+    expect(month[0][0]).toEqual(1);
+
+    // in week 5, index 2 should contain the value 31
+    expect(month[4][2]).toEqual(31);
+  });
+
+  it("Passing an invalid month should throw an error", () => {
+    const cal = new Calendar(2022);
+    expect(() => {
+      cal.getNumDaysInMonth(12);
+    }).toThrowError();
+  });
+});
---
+++ utils/calendar.js
@@ -0,0 +1,87 @@
+// const week = [0,0,0,1,2,3,4]
+export const Months = {
+  JANUARY: 0,
+  FEBRUARY: 1,
+  MARCH: 2,
+  APRIL: 3,
+  MAY: 4,
+  JUNE: 5,
+  JULY: 6,
+  AUGUST: 7,
+  SEPTEMBER: 8,
+  OCTOBER: 9,
+  NOVEMBER: 10,
+  DECEMBER: 11
+};
+
+export const Days = {
+  SUNDAY: 0,
+  MONDAY: 1,
+  TUESDAY: 2,
+  WEDNESDAY: 3,
+  THURSDAY: 4,
+  FRIDAY: 5,
+  SATURDAY: 6
+};
+
+export class Calendar {
+
+  constructor(year) {
+    this.year = year;
+  }
+
+  getNumDaysInMonth(month) {
+    // passing day 0 to Date() constructor sets it as the
+    // previous month's last day
+    if (month < Months.JANUARY || month > Months.DECEMBER) {
+      throw new Error("Invalid month: " + month);
+    }
+
+    const d = new Date(this.year, month + 1, 0);
+    return d.getDate();
+  }
+
+  getDayFromDate(month, day) {
+    if (month < Months.JANUARY || month > Months.DECEMBER) {
+      throw new Error("Invalid month: " + month);
+    }
+    // return which day the date falls on
+    return new Date(this.year, month, day).getDay();
+  }
+
+  createMonthlyCalendar(month) {
+    if (month < Months.JANUARY || month > Months.DECEMBER) {
+      throw new Error("Invalid month: " + month);
+    }
+    // fill in the array starting with the date 1 in the correct day 
+    // position. Then just fill in the array until the last day of the month.
+    // starting a new week after every saturday.
+
+    // to create the calendar we figure out which day the month starts on.
+    // then we find out how many days are in the month.
+    const startingDay = this.getDayFromDate(month, 1);
+    const numDays = this.getNumDaysInMonth(month);
+
+    const monthlyCalendar = [];
+    let currDate = 1;
+    let currDayOfWeek = startingDay; // used as index in the week arrays
+
+    while (currDate <= numDays) {
+      // we create an array filled with 7 zeroes to represent the week.
+      // each index is a day from sunday to saturday and will hold the
+      // actual date
+      const week = [0, 0, 0, 0, 0, 0, 0];
+
+      for (let i = currDayOfWeek; i <= Days.SATURDAY; i++) {
+        if (currDate > numDays) {
+          break; // make sure not to go passed the actual num days in the month
+        }
+        week[i] = currDate++;
+      }
+      monthlyCalendar.push(week);
+      currDayOfWeek = Days.SUNDAY; //start the week over
+    }
+
+    return monthlyCalendar;
+  }
+}
