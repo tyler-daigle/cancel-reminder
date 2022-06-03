9f34a9d1b8b81506759a4d4b37c8d13968738825
Tyler D <tyler@tdaigle.com>
2022-06-03 15:12:18 -0400

    Added firebase and config file

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCskGz899e_SWKffSvrnnZSXdywUYcbKqU",
+  authDomain: "cancel-reminder.firebaseapp.com",
+  projectId: "cancel-reminder",
+  storageBucket: "cancel-reminder.appspot.com",
+  messagingSenderId: "997103952005",
+  appId: "1:997103952005:web:6df2164aefd5ca709fc9ec",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
