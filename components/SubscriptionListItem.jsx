53d6b7707ecbb6d735743a799ad97202f5e95394
Tyler D <tyler@tdaigle.com>
2022-06-03 15:44:21 -0400

    MonthlyTotal.jsx - totalItems() converts the cents to dollars

--- components/SubscriptionListItem.jsx
+++ components/SubscriptionListItem.jsx
@@ -4,13 +4,19 @@ import CancelButton from "@components/UI/CancelButton";
 import EditButton from "@components/UI/EditButton";
 
 export default function SubscriptionListItem({ subscriptionItem }) {
-  const { name, billingPeriod, price, startDate, isActive, logo } = subscriptionItem;
+  const { name, billingPeriod, price, startDate, isActive, logo } =
+    subscriptionItem;
   const { daysLeftString, expirationDate } = numberDaysTillExpire(startDate);
 
   return (
     <li className={styles.subscriptionListItem}>
       <EditButton />
-      <img className={styles.subscriptionLogo} width="75" height="60" src={`/logos/${logo}`} />
+      <img
+        className={styles.subscriptionLogo}
+        width="75"
+        height="60"
+        src={`/logos/${logo}`}
+      />
       <span className={styles.subscriptionName}>{name}</span>
       <span className={styles.numberDaysLeft}>{daysLeftString}</span>
       <span className={styles.expirationDate}>{expirationDate}</span>
@@ -20,5 +26,6 @@ export default function SubscriptionListItem({ subscriptionItem }) {
 }
 
 function numberDaysTillExpire(date) {
+  // TODO: calculate the actual number of days till renewal.
   return { daysLeftString: "10 Days", expirationDate: "7/21/22" };
 }
