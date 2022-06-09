96519f7523b5834602c4975e223ec283ca31ea85
Tyler D <tyler@tdaigle.com>
2022-06-09 11:38:06 -0400

    removed MainContainer from add_subscription page

--- pages/add_subscription.js
+++ pages/add_subscription.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import MainContainer from "@components/UI/MainContainer";
+import CenterContainer from "@components/UI/CenterContainer";
 import SubscriptionForm from "@components/SubscriptionForm";
 
 export default function AddSubscription() {
@@ -10,9 +10,9 @@ export default function AddSubscription() {
       </Head>
       {/* TODO: Remove the MainContainer - the add page doesn't need grid positioning */}
       <h2>Add A Subscription</h2>
-      <MainContainer>
+      <CenterContainer>
         <SubscriptionForm />
-      </MainContainer>
+      </CenterContainer>
     </>
   );
 }
