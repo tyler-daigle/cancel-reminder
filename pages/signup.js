48816d4241cf68a86a2fd6a1f42bfd56282f2852
Tyler D <tyler@tdaigle.com>
2022-06-09 05:27:17 -0400

    added signup page

---
+++ pages/signup.js
@@ -0,0 +1,11 @@
+import styles from "@styles/pages/Signup.module.css";
+
+import MainContainer from "@components/UI/MainContainer";
+
+export default function SignUp() {
+  return (
+    <MainContainer>
+      <div className={styles.signupContainer}></div>
+    </MainContainer>
+  );
+}
